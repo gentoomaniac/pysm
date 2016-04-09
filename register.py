6e8aa540e9a1eae2198c7d840ee431bb9987a0a1
Marco Siebecke <gentoomaniac@gmail.com>
2016-04-09 13:45:31 +0200

    register class now uses propper datatypes

--- register.py
+++ register.py
@@ -1,59 +1,57 @@
+from datatypes.dword import Dword
+
 class Register(object):
     """ Base class for all registers
     """
 
     def __init__(self):
-        self.__value = 0
+        self.__dword = Dword()
 
     @property
     def full(self):
         """ returns full register
         """
-        return  self.__value & 0xffffffff
+        return self.__dword.value
 
     @full.setter
     def full(self, value):
         if not isinstance(value, int):
             raise ValueError("Registers can only take integers")
-        self.__value = value & 0xffffffff
+        self.__dword.value = value
 
     @property
     def half(self):
         """ returns lowest 16 bit
         """
-        return  self.__value & 0xffff
+        return self.__dword.low.value
 
     @half.setter
     def half(self, value):
         if not isinstance(value, int):
             raise ValueError("Registers can only take integers")
-        self.__value = ( self.__value | 0x0000ffff) & ((value & 0xffff) | 0xffff0000)
+        self.__dword.low.value = value
 
     @property
     def high(self):
         """ returns highest 8 bit of the lower 16 bit
         """
-        return ( self.__value & 0xff00) >> 8
+        return self.__dword.low.high.value
 
     @high.setter
     def high(self, value):
         if not isinstance(value, int):
             raise ValueError("Registers can only take integers")
-        limited_value = value & 0xff  # limit the value to 8 bits
-        shifted_value = limited_value << 8  # shift those bits to the right position
-        masked_value = shifted_value | 0xffff00ff  # mask the bits we don't want to update
 
-        eax_masked =  self.__value | 0x0000ff00  # mask out the bits we want to update
-        self.__value = masked_value & eax_masked  # merge the two masks into one new updated register
+        self.__dword.low.high.value = value
 
     @property
     def low(self):
         """ returns lowest 8 bit of the register
         """
-        return  self.__value & 0xff
+        return self.__dword.low.low.value
 
     @low.setter
     def low(self, value):
         if not isinstance(value, int):
             raise ValueError("Registers can only take integers")
-        self.__value = ( self.__value | 0x000000ff) & ((value & 0xff) | 0xffffff00)
+        self.__dword.low.low.value = value
